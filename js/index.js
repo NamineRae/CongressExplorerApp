36a850f22315c433a670a34aecc0e8819867290b
Madelyn Daynes <maddy.daynes@gmail.com>
2021-07-10 23:43:49 -0600

    trying to build functions to combine the arrays, but there are errors

---
+++ js/index.js
@@ -0,0 +1,41 @@
+import {senators} from '.senators'
+import {representatives} from '.representatives'
+
+//combine the data in senators.js and represntatives.js
+function simplifyCongress(reps, sens) {
+console.log(reps)
+let finalArray = [];
+
+    //combine the arrays
+    reps.forEach(rep) => {
+        
+
+        //info we want
+        let simpleRep = {
+            fullName: ${rep.first_name} ${rep.last_name},
+            shortTitle: rep.short_title;
+            seniority: rep.seniority;
+        }
+
+    finalArray.push(simpleRep)
+    }
+
+    sens.forEach(rep) => {
+        
+
+        //info we want
+        let simpleSen = {
+            fullName: ${sen.first_name} ${sen.last_name},
+            shortTitle: sen.short_title;
+            seniority: sen.seniority;
+        }
+
+    finalArray.push(simpleSen)
+    }
+
+    //return the final array
+    return finalArray
+}
+
+let simpleCongressPeople = simplifyCongress(representatives, senators);
+console.log(simpleCongressPeople)
